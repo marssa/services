c429c8354b034aee64f206e09f6bc1d01d6b0544
Clayton Tabone <taboneclayton@gmail.com>
2012-04-18 16:11:14 +0200

    Updated License header

--- src/main/java/mise/marssa/services/scheduling/MTimerTask.java
+++ src/main/java/mise/marssa/services/scheduling/MTimerTask.java
@@ -1,5 +1,17 @@
 /**
- * 
+ * Copyright 2012 MARSEC-XL International Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions ands
+ * limitations under the License.
  */
 package mise.marssa.services.scheduling;
 
@@ -46,7 +58,7 @@ public abstract class MTimerTask extends TimerTask {
 	public MString getTaskName() {
 		return taskName;
 	}
-	
+
 	public void setTaskName(MString taskName) {
 		this.taskName = taskName;
 	}
